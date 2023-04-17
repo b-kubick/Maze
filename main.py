6b0a20ef4b13f79ff7a2309aebac8695db5488c2
raziehyria <71193841+raziehyria@users.noreply.github.com>
2023-04-17 18:16:37 -0400

    added comments and simple features

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from maze import Maze
 from mazeSolver import MazeSolver
 from mazeGUI import MazeGUI
+import random
+
+def main():
+    x = 15
+    y = 15
+    start=(random.randint(0,x-1 ), random.randint(0, y-1)) 
+    goal=(random.randint(0, x-1), random.randint(0, y-1))
+    
+    # Create a maze with default width of 15 and random start and goal points
+    maze = Maze(x, y, start, goal)
+    
+    solver = MazeSolver(maze)
+    # Create a MazeGUI object with the default maze, solver, and cell size
+    gui = MazeGUI(maze, solver)
 
 if __name__ == "__main__":
-    width, height = 10, 10
-    start, goal = (1, 1), (8, 8)
+    # shows the full grid
+    #width, height = 15, 15
+    #adjusts the goal
+    #start, goal = (1, 1), (13, 13)
+    main()
 
-    maze = Maze(width, height, start, goal)
-    solver = MazeSolver(maze)
-
-    gui = MazeGUI(maze, solver)
+ 
--- mazeGUI.py
+++ mazeGUI.py
@@ -5,30 +5,68 @@ from mazeSolver import MazeSolver
 
 class MazeGUI:
     def __init__(self, maze, solver, cell_size=30):
+         # Initialize the MazeGUI class with the specified maze, solver, and cell size
         self.maze = maze
         self.solver = solver
         self.cell_size = cell_size
-
+        
+        # Create the root window for the GUI    
         self.root = tk.Tk()
+        self.root.title("Maze Solver")
+        
+        # Set maximum/minimum window size
+        self.root.minsize(700, 600)
+        self.root.maxsize(700, 600)
+        
+        # Create a BooleanVar to track whether the maze has been solved
         self.solved = tk.BooleanVar()
         self.solved.set(False)
-
+        # Calculate the width and height of the canvas based on the maze size and cell size
+        canvas_width = maze.width * cell_size
+        canvas_height = maze.height * cell_size
+        
+        # Calculate the screen dimensions
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        
+        # Calculate the center coordinates of the screen
+        x_center = int(screen_width / 2 - 700 / 2)
+        y_center = int(screen_height / 2 - 600 / 2)
+        
+        # Set the geometry of the root window to center it on the screen
+        self.root.geometry(f"700x600+{x_center}+{y_center}")
+        
+        # Create a canvas to draw the maze
         self.canvas = tk.Canvas(self.root, width=maze.width * cell_size, height=maze.height * cell_size)
         self.canvas.pack()
-
+        
+        # Create a button to solve the maze
         self.solve_button = tk.Button(self.root, text="Solve", command=self.solve_and_draw_solution)
         self.solve_button.pack()
-
+        
+        # Create a button to generate a new maze
         self.refresh_button = tk.Button(self.root, text="Refresh", command=self.generate_maze)
         self.refresh_button.pack()
-
+        
+        # Create a button to exit the application
         self.exit_button = tk.Button(self.root, text="Exit", command=self.exit_app)
         self.exit_button.pack()
-
+        
+        # Generate and draw the initial maze
         self.generate_maze()
+        
+        # Start the main event loop for the GUI
         self.root.mainloop()
-
-    def draw_maze(self):
+    
+    def goal_reached(self):
+        # Display "Maze solved!" in green on the console when the goal is reached
+        self.canvas.create_text(self.maze.goal[1] * self.cell_size + self.cell_size // 2,
+        self.maze.goal[0] * self.cell_size + self.cell_size // 2, 
+        text="Maze solved!", fill="green")
+        print("\033[32mMaze solved!\033[0m")
+        
+    def draw_maze(self): 
+        # Draw the maze
         for i, row in enumerate(self.maze.grid):
             for j, cell in enumerate(row):
                 x1, y1 = j * self.cell_size, i * self.cell_size
@@ -44,23 +82,32 @@ class MazeGUI:
                     color = "white"
 
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color)
-
+        
+        # Add a border around the maze
+        x1, y1 = 0, 0
+        x2, y2 = self.maze.width * self.cell_size, self.maze.height * self.cell_size
+        self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", width=2)
+        
         if self.solved.get():
             self.draw_solution(self.solver.get_solution_path())
 
     def solve_and_draw_solution(self):
+        # Solve the maze and redraw the canvas with the solution
         if not self.solved.get():
             self.solver.solve()
             self.draw_solution(self.solver.get_solution_path())
             self.solve_button.config(text="Clear")
             self.solved.set(True)
+    
+            
         else:
             self.canvas.delete("path")
             self.solve_button.config(text="Solve")
             self.solved.set(False)
             self.solver.reset()  # Reset the solver when clearing the solution
-
+            
     def draw_solution(self, solution_path):
+         # Draw the solution on the canvas
         for i, (row, col) in enumerate(solution_path):
             x1, y1 = col * self.cell_size, row * self.cell_size
             x2, y2 = x1 + self.cell_size, y1 + self.cell_size
@@ -73,12 +120,13 @@ class MazeGUI:
                 color = "brown"  # Solution path
 
             self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, tags="path")
+        self.goal_reached()
 
     def generate_maze(self):
+        # Generate a new maze and redraw the canvas
         self.maze.generate_maze()
         self.solver = MazeSolver(self.maze)  # Reset the solver's internal state
-        # self.solver.maze = self.maze
-        self.solved.set(False)
+        self.solved.set(False) # Reset the solved flag to False
         self.solve_button.config(text="Solve")
         self.canvas.delete("path")
         self.draw_maze()
