1b975d8ab0afa8277dbd38a8ffb737ea5829ba28
b-kubick <b.l.kubick@gmail.com>
2023-04-17 10:37:38 -0400

    changes in mazSolver.py It now used A* algorithm instead of a modified version of the Bellman-Ford algorithm

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,31 +1,40 @@
+import heapq
+
+
 class MazeSolver:
     def __init__(self, maze):
         self.maze = maze
         self.height = maze.height
         self.width = maze.width
-        self.distance = [[float("inf")] * self.width for _ in range(self.height)]
         self.predecessor = [[None] * self.width for _ in range(self.height)]
-
-    def initialize_single_source(self):
-        start_row, start_col = self.maze.start
-        self.distance[start_row][start_col] = 0
-
-    def relax(self, current, neighbor):
-        row, col = current
-        n_row, n_col = neighbor
-        if self.distance[n_row][n_col] > self.distance[row][col] + 1:
-            self.distance[n_row][n_col] = self.distance[row][col] + 1
-            self.predecessor[n_row][n_col] = current
+        
+    # calculates the manhattan distance between the current cell(row, col) and the goal cell(goal_row, goal_col)
+    def heuristic(self, row, col):
+        goal_row, goal_col = self.maze.goal
+        return abs(row - goal_row) + abs(col - goal_col)
 
     def solve(self):
-        self.initialize_single_source()
-        for _ in range(self.height * self.width):
-            for row in range(self.height):
-                for col in range(self.width):
-                    current = (row, col)
-                    if self.maze.is_valid_coordinate(row, col):
-                        for neighbor in self.maze.get_neighbors(row, col):
-                            self.relax(current, neighbor)
+        start_row, start_col = self.maze.start
+        open_list = [(0, start_row, start_col)]  # Priority queue with (priority, row, col) tuples
+        heapq.heapify(open_list)
+
+        g_costs = [[float("inf")] * self.width for _ in range(self.height)]
+        g_costs[start_row][start_col] = 0
+
+        while open_list:
+            _, current_row, current_col = heapq.heappop(open_list)
+
+            if (current_row, current_col) == self.maze.goal:
+                break
+
+            for neighbor_row, neighbor_col in self.maze.get_neighbors(current_row, current_col):
+                tentative_g_cost = g_costs[current_row][current_col] + 1
+
+                if tentative_g_cost < g_costs[neighbor_row][neighbor_col]:
+                    g_costs[neighbor_row][neighbor_col] = tentative_g_cost
+                    f_cost = tentative_g_cost + self.heuristic(neighbor_row, neighbor_col)
+                    heapq.heappush(open_list, (f_cost, neighbor_row, neighbor_col))
+                    self.predecessor[neighbor_row][neighbor_col] = (current_row, current_col)
 
     def get_solution_path(self):
         path = []
